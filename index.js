aa26939b87942578739d5ca4c0ea41fca6a0e07e
Kushak-S <101641825+Kushak-S@users.noreply.github.com>
2023-05-19 19:49:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const userRoutes = require("./routers/user.routes");
 app.use(express.json());
 const port = 3001;
 
-app.get('/',async (req,res)=>{res.status(200).send('SERVER IS RUNNING....')})
+app.get('/',async (req,res)=>{res.status(200).send('<html><body><h1>Server Running ...</h1><h2>Github link: https://github.com/Kushak-S/bajajfv_test</h2><h2>API Link: https://determined-foal-sari.cyclic.app/bghl</h2></body></html>')})
 app.use(userRoutes);
 
 app.listen(port, () => {
