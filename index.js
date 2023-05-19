a1ac756b7d259935365f82b7430d0260126b9da0
Kushak-S <101641825+Kushak-S@users.noreply.github.com>
2023-05-19 19:42:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const userRoutes = require("./routers/user.routes");
 app.use(express.json());
 const port = 3001;
 
+app.get('/',async (req,res)=>{res.status(200).send('SERVER IS RUNNING....')})
 app.use(userRoutes);
 
 app.listen(port, () => {
